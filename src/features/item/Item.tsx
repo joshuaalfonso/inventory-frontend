62df75714ed6dfe13b51a108e561340e58059d07
Joshua Alfonso <Joshua025icloud@gmail.com>
2026-03-25 20:58:57 +0800

    fix combo box scroll smooth

--- src/features/item/Item.tsx
+++ src/features/item/Item.tsx
@@ -1,48 +1,12 @@
-import { Button, Combobox, Heading, Portal, useFilter, useListCollection} from "@chakra-ui/react"
+import { Button, Heading} from "@chakra-ui/react"
 import { useItemDialogStore } from "./hooks/useItemDialogStore";
 import ItemDialog from "./components/dialog/ItemDialog";
-import { useBrands } from "@/features/brand/hooks/useBrand";
-import { useMemo, useRef } from "react";
-import { useVirtualizer } from "@tanstack/react-virtual";
-import { flushSync } from "react-dom";
+
 
 
 const Item = () => {
 
   const openDialog = useItemDialogStore(state => state.openDialog);
-
-   const { brands } = useBrands();
-  
-      const contentRef = useRef<HTMLDivElement | null>(null)
-  
-      const { startsWith } = useFilter({ sensitivity: "base" });
-  
-      const stableBrands = useMemo(() => brands ?? [], [brands])
-  
-      const { collection, filter, reset: resetCollection } = useListCollection({
-          initialItems: stableBrands ?? [],
-          filter: startsWith,
-          itemToString: (item) => item.brand_name,
-          itemToValue: (item) => String(item.brand_id),
-      });
-  
-      const virtualizer = useVirtualizer({
-          count: collection.size,
-          getScrollElement: () => contentRef.current,
-          estimateSize: () => 36,
-          overscan: 10,
-           scrollPaddingEnd: 32,
-      })
-  
-      const handleScrollToIndexFn = (details: { index: number }) => {
-          flushSync(() => {
-              virtualizer.scrollToIndex(details.index, {
-                  align: "center",
-                  behavior: "auto",
-                  // behavior: "smooth",
-              })
-          })
-      }
 
   return (
     <>
@@ -57,76 +21,12 @@ const Item = () => {
         Create
       </Button>
 
-      <Combobox.Root
-          collection={collection}
-          onInputValueChange={(e) => filter(e.inputValue)}
-          scrollToIndexFn={handleScrollToIndexFn}
-          width="320px"
-          positioning={{ strategy: "fixed" }}
-      >
-          <Combobox.Label>Select framework</Combobox.Label>
-          <Combobox.Control>
-          <Combobox.Input placeholder="Type to search" />
-          <Combobox.IndicatorGroup>
-              <Combobox.ClearTrigger />
-              <Combobox.Trigger onClick={resetCollection} />
-          </Combobox.IndicatorGroup>
-          </Combobox.Control>
-      <Portal>
-          <Combobox.Positioner>
-          <Combobox.Content 
-              ref={contentRef}
-              // style={{
-              //     maxHeight: "240px",
-              //     overflow: "auto",
-              // }}
-          >
-              <div
-                style={{
-                    height: `${virtualizer.getTotalSize()}px`,
-                    width: "100%",
-                    position: "relative",
-                }}
-              >
-              {virtualizer.getVirtualItems().map((virtualItem) => {
-                  const item = collection.items[virtualItem.index]
-                  return (
-                  <Combobox.Item
-                      key={item.brand_id}
-                      item={item}
-                      style={{
-                      position: "absolute",
-                      top: 0,
-                      left: 0,
-                      width: "100%",
-                      height: `${virtualItem.size}px`,
-                      transform: `translateY(${virtualItem.start}px)`,
-                      whiteSpace: "nowrap",
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
-                      }}
-                  >
-                      <Combobox.ItemText truncate>
-                          {/* <span aria-hidden style={{ marginRight: 4 }}>
-                              {item.emoji}
-                          </span> */}
-                          {item.brand_name}
-                      </Combobox.ItemText>
-                      <Combobox.ItemIndicator />
-                  </Combobox.Item>
-                  )
-              })}
-              </div>
-          </Combobox.Content>
-          </Combobox.Positioner>
-      </Portal>
-      </Combobox.Root>
-
       <ItemDialog />
-
       
     </>
   )
 }
+
+
 
 export default Item
--- src/features/item/components/dialog/ItemDialog.tsx
+++ src/features/item/components/dialog/ItemDialog.tsx
@@ -1,6 +1,11 @@
-import { Button, CloseButton, Dialog, Field, Fieldset, Heading, Input, Portal, Stack, Text } from "@chakra-ui/react";
+import { Button, CloseButton, Combobox, DatePicker, Dialog, Field, Fieldset, Heading, Input, Portal, Stack, Text, useFilter, useListCollection } from "@chakra-ui/react";
 import { useItemDialogStore } from "../../hooks/useItemDialogStore";
 import { useForm, type SubmitHandler } from "react-hook-form";
+import { useBrands } from "@/features/brand/hooks/useBrand";
+import { useMemo, useRef } from "react";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { flushSync } from "react-dom";
+import { LuCalendar } from "react-icons/lu";
 
 
 
@@ -31,7 +36,37 @@ const ItemDialog = () => {
 
     console.log('item dialog')
 
-   
+    const { brands } = useBrands();
+
+    const contentRef = useRef<HTMLDivElement | null>(null)
+
+    const { startsWith } = useFilter({ sensitivity: "base" });
+
+    const stableBrands = useMemo(() => brands ?? [], [brands])
+
+    const { collection, filter, reset: resetCollection } = useListCollection({
+        initialItems: stableBrands ?? [],
+        filter: startsWith,
+        itemToString: (item) => item.brand_name,
+        itemToValue: (item) => String(item.brand_id),
+    });
+
+    const virtualizer = useVirtualizer({
+        count: collection.size,
+        getScrollElement: () => contentRef.current,
+        estimateSize: () => 28,
+        overscan: 10,
+        scrollPaddingEnd: 32,
+    })
+
+    const handleScrollToIndexFn = (details: { index: number }) => {
+        flushSync(() => {
+            virtualizer.scrollToIndex(details.index, {
+                align: "center",
+                behavior: "auto",
+            })
+        })
+    }
 
 
     const { 
@@ -138,6 +173,95 @@ const ItemDialog = () => {
                                         
                                         <Field.ErrorText>This is an error text</Field.ErrorText>
                                     </Field.Root> */}
+
+                                    <Combobox.Root
+                                        collection={collection}
+                                        onInputValueChange={(e) => filter(e.inputValue)}
+                                        scrollToIndexFn={handleScrollToIndexFn}
+                                        width=""
+                                    >
+                                        <Combobox.Label>Select framework</Combobox.Label>
+                                        <Combobox.Control>
+                                            <Combobox.Input placeholder="Type to search" />
+                                            <Combobox.IndicatorGroup>
+                                            <Combobox.ClearTrigger />
+                                            <Combobox.Trigger onClick={resetCollection} />
+                                            </Combobox.IndicatorGroup>
+                                        </Combobox.Control>
+                                        {/* <Portal> */}
+                                            <Combobox.Positioner>
+                                            <Combobox.Content 
+                                                ref={contentRef}
+                                                maxH="200px"
+                                                overflowY="auto"
+                                            >
+                                                <div
+                                                    style={{
+                                                        height: `${virtualizer.getTotalSize()}px`,
+                                                        width: "100%",
+                                                        position: "relative",
+                                                    }}
+                                                >
+                                                {virtualizer.getVirtualItems().map((virtualItem) => {
+                                                    const item = collection.items[virtualItem.index]
+                                                    return (
+                                                    <Combobox.Item
+                                                        key={item.brand_id}
+                                                        item={item}
+                                                        style={{
+                                                        position: "absolute",
+                                                        top: 0,
+                                                        left: 0,
+                                                        width: "100%",
+                                                        height: `${virtualItem.size}px`,
+                                                        transform: `translateY(${virtualItem.start}px)`,
+                                                        whiteSpace: "nowrap",
+                                                        overflow: "hidden",
+                                                        textOverflow: "ellipsis",
+                                                        }}
+                                                    >
+                                                        <Combobox.ItemText truncate>
+                                                            {item.brand_name}
+                                                        </Combobox.ItemText>
+                                                        <Combobox.ItemIndicator />
+                                                    </Combobox.Item>
+                                                    )
+                                                })}
+                                                </div>
+                                            </Combobox.Content>
+                                            </Combobox.Positioner>
+                                        {/* </Portal> */}
+                                    </Combobox.Root>
+
+                                    <DatePicker.Root maxWidth="">
+                                        <DatePicker.Label>Date of birth</DatePicker.Label>
+                                        <DatePicker.Control>
+                                            <DatePicker.Input />
+                                            <DatePicker.IndicatorGroup>
+                                            <DatePicker.Trigger>
+                                                <LuCalendar />
+                                            </DatePicker.Trigger>
+                                            </DatePicker.IndicatorGroup>
+                                        </DatePicker.Control>
+                                        {/* <Portal> */}
+                                            <DatePicker.Positioner>
+                                            <DatePicker.Content>
+                                                <DatePicker.View view="day">
+                                                <DatePicker.Header />
+                                                <DatePicker.DayTable />
+                                                </DatePicker.View>
+                                                <DatePicker.View view="month">
+                                                <DatePicker.Header />
+                                                <DatePicker.MonthTable />
+                                                </DatePicker.View>
+                                                <DatePicker.View view="year">
+                                                <DatePicker.Header />
+                                                <DatePicker.YearTable />
+                                                </DatePicker.View>
+                                            </DatePicker.Content>
+                                            </DatePicker.Positioner>
+                                        {/* </Portal> */}
+                                    </DatePicker.Root>
 
                                     
 
