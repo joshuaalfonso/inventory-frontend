4cb84cdbeaeeb0c490b54cd65906634dd5151399
Joshua Alfonso <joshua025icloud@gmail.com>
2026-05-05 15:35:00 +0800

    pending po staletime

--- src/features/purchase-order/hooks/usePendingPurchaseOrder.ts
+++ src/features/purchase-order/hooks/usePendingPurchaseOrder.ts
@@ -9,9 +9,7 @@ import { getPendingPurchaseOrderApi } from "../purchaseOrder.api";
 export const usePendingPurchaseOrders = () => {
   const { data: pendingPurchaseOrders, isPending, error } = useQuery({
     queryKey: ["pendingPurchaseOrders"],
-    queryFn: getPendingPurchaseOrderApi,
-    staleTime: 1000 * 60 * 5, 
-    gcTime: 1000 * 60 * 30 
+    queryFn: getPendingPurchaseOrderApi
   });
 
   return { pendingPurchaseOrders, isPending, error }
